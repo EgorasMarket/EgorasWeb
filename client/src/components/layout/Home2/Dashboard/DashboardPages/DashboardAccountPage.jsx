476b613010f382fd742a3160a517394d04a32e6a
Buike369 <chukwubuikekingsley369@gmail.com>
2022-01-17 07:35:04 -0800

    fixing some issue

--- client/src/components/layout/Home2/Dashboard/DashboardPages/DashboardAccountPage.jsx
+++ client/src/components/layout/Home2/Dashboard/DashboardPages/DashboardAccountPage.jsx
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {API_URL2 as api_url2} 
+ from "../../../../../actions/types";
 import Stack from "@mui/material/Stack";
 import EditIcon from "@mui/icons-material/Edit";
-import AvatarSelector from "react-avatar-selector";
 // import poodle from "../../img/profile_img.jpeg";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 // import  {useLocal}
 import { useLocalStorage } from "../../Activation/useLocalStorage";
-// import TextField from "@mui/material/TextField";
-// import AdapterDateFns from "@mui/lab/AdapterDateFns";
-// import LocalizationProvider from "@mui/lab/LocalizationProvider";
-// import TimePicker from "@mui/lab/TimePicker";
-// import DateTimePicker from "@mui/lab/DateTimePicker";
-// import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
-// import MobileDatePicker from "@mui/lab/MobileDatePicker";
+import jwt from "jsonwebtoken";
+
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -24,8 +21,17 @@ import "../DashboardStyles/dashboard_account.css";
 import { connect } from "react-redux";
 import { sumitGenderAndDate, nextOfKING } from "../../../../../actions/auth";
 import { setAlert } from "../../../../../actions/alert";
+// import {getNaame} from "../../../Signup/signup"
 
-function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
+function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING , auth}) {
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+
   const [tokens, setTokens] = useState({ gender: "", dateOfBirth: "" });
 
   const [nextKin, setNextKin] = useState({
@@ -33,11 +39,109 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
     lastname: "",
     email: "",
     phoneNumber: "",
-    gender1: "",
     relationship: "",
+    gender: "",
+   
   });
 
-  const { firstname, lastname, email, gender1, relationship, phoneNumber } =
+  //    useEffect(()=>{
+
+  
+  //      const userInfo =
+  //     () =>
+  //     async (dispatch) => {
+  //       const config = {
+  //         headers: {
+  //           Accept: "*",
+  //           "Access-Control-Allow-Origin": "*",
+  //         },
+  //       };
+    
+     
+    
+  //       try {
+  //         const res = await axios.get(
+  //           api_url2 + "/v1/user/info",
+            
+  //           config
+  //         );
+  //         console.log(res);
+    
+  //         return {
+  //           success: true,
+  //           data: res.data,
+  //         };
+  //       } catch (err) {
+  //         console.log(err.response);
+    
+  //         return {
+  //           success: false,
+  //           data: err.response,
+  //         };
+  //       }
+  //     };
+
+  // },[])
+
+//   useEffect(() => {
+  
+//     axios
+//       .get(
+//         api_url2+
+//           "/v1/user/info",
+//         null,
+//         config
+//       )
+//       .then((data) => {
+//         console.log(data);
+        
+//       });
+  
+
+//   // }
+// }, []);
+
+
+
+useEffect(() => {
+  // fetchDepositLinks();
+  console.log(auth);
+  if (auth.user !== null) {
+    var todecoded = auth.user;
+    var decoded = jwt.decode(todecoded, {
+      complete: true,
+    });
+    setUserName(decoded.payload.user.username);
+    // setIsLoggedIn(true);
+
+    console.log(decoded.payload);
+    var eee = decoded.payload.exp * 1000;
+    var exp = new Date(eee).getTime();
+
+    // if (currentTimestamp >= exp) {
+    //   // //console.log('ex');
+    //   localStorage.removeItem("token");
+    //   window.location.href = "/login";
+    // }
+  }
+}, [auth]);
+
+
+
+
+  // const [userInfoUpdate,setUserInfoUpdate]=useState({
+  //   firstname:"",lastname:"",phoneNumber:"",email:"",BVN:"",
+  // })
+
+
+  // const {firstname1,lastname1,phoneNumber1,email1,BVN1} = userInfoUpdate;
+
+  const [userName,setUserName]=useState({user:""});
+  const [nameUpdate,setNameUpdate]= useState("");
+
+  const {user}=userName;
+
+  const { firstname, lastname, email, phoneNumber,relationship, gender1,  } =
     nextKin;
 
   const { gender, dateOfBirth } = tokens;
@@ -48,17 +152,28 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
 
   const onChangeFor2 = (e) => {
     setNextKin({ ...nextKin, [e.target.name]: e.target.value });
+    console.log(nextKin)
   };
+
+  // const onChangeFor4 =(e)=>{
+  //   setUserInfoUpdate({...userInfoUpdate,[e.target.name]:e.target.value})
+  // }
+
+  // const updateUser =()=>{
+  //   setUserName()
+  // }
 
   // const [value, setValue] = useState("1997-02-09");
   // const [email, setEmail] = useState("samuelify225@gmail.com");
   const [bvnNum, setBvnNum] = useState("23745672845");
   const [phoneNo, setPhoneNo] = useState("+2348164020234");
   //   const [value, setValue] = useState(new Date("2014-02-09"));
-  const [age, setAge] = React.useState({ relationship });
+  const [age, setAge] = React.useState("");
 
   const handleChange = (event) => {
     setAge(event.target.value);
+    setNextKin({ ...nextKin, [event.target.name]: event.target.value });
+    console.log(nextKin)
   };
   const [activeBg, setActiveBg] = useState("accounts");
   const [activeBody, setActiveBody] = useState("");
@@ -86,14 +201,28 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
     }
   };
 
-  const Apple = async (e) => {
+
+  // const updateInfo = async (e) => {
+  //   let res = await userInfo(firstname1,lastname1,phoneNumber1,email1,BVN1);
+
+  //   console.log(res);
+
+  //   if (res.data.data.success === true) {
+  //     console.log("okay Good Server");
+  //   } else {
+  //     setAlert(res.data.data.errors[0].msg, "danger");
+  //   }
+  // };
+
+  const nextOfKINGS = async (e) => {
     let res = await nextOfKING(
       firstname,
       lastname,
       email,
-      gender,
+      phoneNumber,
       relationship,
-      phoneNumber
+      gender,
+     
     );
 
     console.log(res);
@@ -104,6 +233,8 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
       setAlert(res.data.data.errors[0].msg, "danger");
     }
   };
+
+
 
   return (
     <div className="other2" style={{ paddingBottom: "0em" }}>
@@ -299,7 +430,9 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
                     <div className="toggle_body_area1_cont1">
                       <div className="toggle_body_area1_cont1_txts"></div>
                       <div className="toggle_body_area1_cont1_input">
-                        <button className="save_changes_btn">
+                        <button className="save_changes_btn"
+                        onClick={sends}
+                        >
                           Save Changes
                         </button>
                       </div>
@@ -495,18 +628,20 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
                               labelId="demo-simple-select-label"
                               id="demo-simple-select"
                               name="relationship"
-                              value={age}
+                              value={relationship}
                               label="Age"
-                              onChange={handleChange}
+                              // onChange={handleChange}
+                              onChange={onChangeFor2}
                               // onSelect={onChangeFor2}
                             >
-                              <MenuItem value={10}>Mother</MenuItem>
-                              <MenuItem value={20}>Father</MenuItem>
-                              <MenuItem value={30}>Sister</MenuItem>
-                              <MenuItem value={40}>Uncle</MenuItem>
-                              <MenuItem value={50}>Aunt</MenuItem>
-                              <MenuItem value={60}>Brother</MenuItem>
-                              <MenuItem value={70}>Inlaw</MenuItem>
+                              <MenuItem 
+                              name="relationship"value="Mother" >Mother</MenuItem>
+                              <MenuItem value="Father">Father</MenuItem>
+                              <MenuItem value="Sister">Sister</MenuItem>
+                              <MenuItem value="Uncle">Uncle</MenuItem>
+                              <MenuItem value="Aunt">Aunt</MenuItem>
+                              <MenuItem value="Brother">Brother</MenuItem>
+                              <MenuItem value="Inlaw">Inlaw</MenuItem>
                             </Select>
                           </FormControl>
                         </div>
@@ -532,8 +667,8 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
                             name="gender"
                             id="male"
                             // value={Male}
-
-                            onClick={onChangeFor2}
+                             value="Male"
+                            onChange={onChangeFor2}
                           />
                           <label for="male" class="radio" value={gender}>
                             Male
@@ -545,7 +680,8 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
                             name="gender"
                             id="female"
                             // value="female"
-                            onClick={onChangeFor2}
+                            value="Female"
+                            onChange={onChangeFor2}
                           />
                           <label for="female" class="radio" value={gender}>
                             Female
@@ -561,7 +697,7 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
                     <div className="toggle_body_area1_cont1">
                       <div className="toggle_body_area1_cont1_txts"></div>
                       <div className="toggle_body_area1_cont1_input">
-                        <button className="save_changes_btn" onClick={Apple}>
+                        <button className="save_changes_btn" onClick={nextOfKINGS}>
                           Save Changes
                         </button>
                       </div>
@@ -606,7 +742,7 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
                         <span className="toggle_body_area1_cont1_sub_txts"></span>
                       </div>
                       <div className="toggle_body_area1_cont1_input">
-                        {phoneNo} <EditIcon className="edit_icon" />
+                        {phoneNumber} <EditIcon className="edit_icon" />
                       </div>
                     </div>
                     {/* ================= */}
@@ -677,7 +813,14 @@ function DashboardAccountPage({ sumitGenderAndDate, setAlert, nextOfKING }) {
   );
 }
 
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+
 // let  res = await getLogin2(
-export default connect(null, { sumitGenderAndDate, setAlert, nextOfKING })(
+export default connect(mapStateToProps, { sumitGenderAndDate, setAlert, nextOfKING})(
   DashboardAccountPage
 );
