43c3168caac5daf0809e2d69c7cc2301187293c2
Samuel Ifeanyi <samuelify225@gmail.com>
2022-01-19 13:08:53 +0100

    update

--- client/src/components/layout/Home2/Dashboard/DashboardPages/DashboardCart.jsx
+++ client/src/components/layout/Home2/Dashboard/DashboardPages/DashboardCart.jsx
@@ -9,6 +9,7 @@ const lockedItems = [
     total_items: " 19",
     days_left: "28",
     days_left_percent: "82%",
+    total_locked_amount: " 150,000",
     quantity: "1",
     unit_price: "350,000",
 
@@ -18,6 +19,7 @@ const lockedItems = [
     id: 2,
     img: "/img/samsung_tv_555.jpeg",
     name: "Lg smart tv series",
+    total_locked_amount: " 80,000",
     total_items: " 200",
     days_left: "13",
     days_left_percent: "27%",
@@ -29,6 +31,7 @@ const lockedItems = [
     img: "/img/samsung_tv_555.jpeg",
     name: "Iphone 12pro max",
     total_items: " 170",
+    total_locked_amount: " 250,000",
     days_left: "23",
     days_left_percent: "77%",
     quantity: "2",
