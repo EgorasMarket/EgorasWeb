ece894c9961f506da7f0bf546acfeb973ba4bb37
Buike369 <chukwubuikekingsley369@gmail.com>
2022-01-25 10:20:10 -0800

    updating File

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -27,4 +27,4 @@ export const ADD_TO_CART = "ADD_TO_CART";
 export const API_URL = "https://tea.egoras.com";
 // export const API_URL2 = "https://330188b756ae73.lhr.life";
 // export const API_URL2 = "http://localhost:4000";
-export const API_URL2 = "https://db871771d33694.lhr.life";
+export const API_URL2 = "https://54067504a05aaa.lhr.life";
