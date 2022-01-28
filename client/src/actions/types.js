0978968cea58211206b42e8871f8b02b60558081
CYNTAX <81620875+samuel-2001@users.noreply.github.com>
2022-01-28 11:45:38 +0100

    update

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -27,5 +27,5 @@ export const ADD_TO_CART = "ADD_TO_CART";
 export const API_URL = "https://tea.egoras.com";
 // export const API_URL2 = "https://330188b756ae73.lhr.life";
 //<<<<<<< HEAD
-export const API_URL2 = "https:///493ef1a021516b.lhr.life";
+export const API_URL2 = "https:///fc87c0c958c172.lhr.life";
 // =======
