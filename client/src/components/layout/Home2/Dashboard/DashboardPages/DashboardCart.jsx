2bf59c92f2db9173069cacdc05552b699dc8c5f7
CYNTAX <81620875+samuel-2001@users.noreply.github.com>
2022-01-30 11:45:34 +0100

    update

--- client/src/components/layout/Home2/Dashboard/DashboardPages/DashboardCart.jsx
+++ client/src/components/layout/Home2/Dashboard/DashboardPages/DashboardCart.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+
 import DeleteIcon from "@mui/icons-material/Delete";
 import "../DashboardStyles/dashboardCart.css";
 import { connect } from "react-redux";
@@ -36,6 +37,9 @@ const DashboardCart = ({ cart, auth }) => {
       "Content-Type": "application/json",
     },
   };
+  useState(() => {
+    // console.log(body.payload);
+  }, []);
 
   // const fetchFromCart = async (customer_id) => {
   //   console.log('fetchfromCart', customer_id);
@@ -89,103 +93,110 @@ const DashboardCart = ({ cart, auth }) => {
 
       <section className="no-bg">
         <div className="container">
-          <div className="cart_area">
-            <div className="cart_item_num">
-              Cart <span className="cart_num_num">({cart.length} items)</span>
-            </div>
-            <div className="locked_items">
-              <div class="save_prod_deta">
-                <table className="save_item_table">
-                  <thead className="assets-category-titles">
-                    <tr className="assets">
-                      <th className="assets-category-titles-heading1">Item</th>
-                      <th className="assets-category-titles-heading1">
-                        Item Details
-                      </th>
-                      <th className="assets-category-titles-heading1 quant">
-                        Quantity
-                      </th>
-                      <th className="assets-category-titles-heading1 quant">
-                        Unit Price
-                      </th>
-                      <th className="assets-category-titles-heading1_last">
-                        Sub Total
-                      </th>
-                    </tr>
-                  </thead>
 
-                  {cart.map((asset) => (
-                    <tbody
-                      className="save_items_cat  small_height popular-categories"
-                      id="popular-categories"
-                    >
-                      {" "}
-                      <tr className="assets-category-row">
-                        <td className="save_item_data small_height">
-                          <div className="assets-data height_data height_data1">
-                            <img
-                              src={`${api}/${asset.image}`}
-                              alt=""
-                              className="save_item_img_img"
-                            />
-                          </div>
-                        </td>
-                        {/* ======== */}
-                        {/* ======== */}
-                        {/* ======== */}
-                        {/* ======== */}
-                        <td className="save_item_data1">
-                          <div className="save_items_details">
-                            <div className="save_items_details1 small_tetxt">
-                              {asset.product_name}
-                            </div>
-                            <div className="save_total_locked_amount smallll_txxt">
-                              <span className="items_left_amount">
-                                Total Items Left
-                              </span>
-                              {asset.total_items} items
-                            </div>
-                            <div
-                              onClick={() => {
-                                deleteFromCart(asset.product_id);
-                                alert(asset.product_id);
-                              }}
-                              className="remove_from_cart_div"
-                            >
-                              <DeleteIcon className="delete_icon" />
-                              Remove
-                            </div>
-                          </div>
-                        </td>
-                        <td className="save_item_data1b">
-                          <div className="assets-data-name center_name">
-                            {asset.quantity}
-                          </div>
-                        </td>
-                        <td className="save_item_data1b">
-                          <div className="assets-data-name center_name">
-                            ₦{asset.item_amount}
-                          </div>
-                        </td>
-                        <td className="save_item_data1b">
-                          <div className="assets-data-name_last">
-                            ₦{asset.sub_total}
-                          </div>
-                        </td>
+
+          <div className="cart_areas">
+            <div className="cart_area1">
+              <div className="cart_item_num">
+                Cart <span className="cart_num_num">({cart.length} items)</span>
+              </div>
+              <div className="locked_items2">
+                <div class="save_prod_deta">
+                  <table className="save_item_table">
+                    <thead className="assets-category-titles">
+                      <tr className="assets">
+                        <th className="assets-category-titles-heading1">
+                          Item
+                        </th>
+                        <th className="assets-category-titles-heading1">
+                          Item Details
+                        </th>
+                        <th className="assets-category-titles-heading1 quant">
+                          Quantity
+                        </th>
+                        <th className="assets-category-titles-heading1 quant">
+                          Unit Price
+                        </th>
+                        <th className="assets-category-titles-heading1_last">
+                          Sub Total
+                        </th>
                       </tr>
-                    </tbody>
-                  ))}
-                </table>
+                    </thead>
+
+                    {cart.map((asset) => (
+                      <tbody
+                        className="save_items_cat  small_height popular-categories"
+                        id="popular-categories"
+                      >
+                        {" "}
+                        <tr className="assets-category-row">
+                          <td className="save_item_data_cart small_height">
+                            <div className="assets-data height_data height_data1">
+                              <img
+                                src={`${api}/${asset.image}`}
+                                alt=""
+                                className="save_item_img_img"
+                              />
+                            </div>
+                          </td>
+                          {/* ======== */}
+                          {/* ======== */}
+                          {/* ======== */}
+                          {/* ======== */}
+                          <td className="save_item_data1">
+                            <div className="save_items_detailssss">
+                              <div className="save_items_details1 small_tetxt">
+                                {asset.product_name}
+                              </div>
+                              <div className="save_total_locked_amount smallll_txxt">
+                                <span className="items_left_amount">
+                                  Total Items Left
+                                </span>
+                                {asset.total_items} items
+                              </div>
+                              <div
+                                onClick={() => {
+                                  deleteFromCart(asset.product_id);
+                                  alert(asset.product_id);
+                                }}
+                                className="remove_from_cart_div"
+                              >
+                                <DeleteIcon className="delete_icon" />
+                                Remove
+                              </div>
+                            </div>
+                          </td>
+                          <td className="save_item_data1b">
+                            <div className="assets-data-name center_name">
+                              {asset.quantity}
+                            </div>
+                          </td>
+                          <td className="save_item_data1b">
+                            <div className="assets-data-name center_name">
+                              ₦{asset.item_amount}
+                            </div>
+                          </td>
+                          <td className="save_item_data1b">
+                            <div className="assets-data-name_last">
+                              ₦{asset.sub_total}
+                            </div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    ))}
+                  </table>
+                </div>
+                <div className="total_div">
+                  Total: <span className="sum_resu"> ₦{result}</span>
+                </div>
               </div>
-              <div className="total_div">
-                Total: <span className="sum_resu"> ₦{result}</span>
+              <div className="checkout_btns">
+                <button className="checkout_btn1" onClick={OpenModal}>
+                  Proceed to Checkout{" "}
+                </button>
               </div>
             </div>
-            <div className="checkout_btns">
-              <button className="checkout_btn1" onClick={OpenModal}>
-                Proceed to Checkout{" "}
-              </button>
-            </div>
+            <div className="cart_area2"></div>
           </div>
         </div>
       </section>
