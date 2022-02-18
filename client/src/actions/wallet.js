a0ada51558997e6810921f69e1b02e9969ed5915
CYNTAX <81620875+samuel-2001@users.noreply.github.com>
2022-02-18 14:54:08 +0100

    update

--- client/src/actions/wallet.js
+++ client/src/actions/wallet.js
@@ -13,29 +13,23 @@ import {
 // import setAuthToken from "../utils/setAuthToken";
 import setAuthToken from "../utils/setAuthToken";
 
-
-
-export const createWallet = (customer_id) => async (dispatch) => {
-//   console.log(payload1, pin);
+export const createWallet = (customer_id, tokenSymbol) => async (dispatch) => {
+  //   console.log(payload1, pin);
   const config = {
     headers: {
       "Content-Type": "application/json",
     },
   };
 
+  //   "customer_id": "string",
+  //   "walletAddress": "string",
+  //   "secKey": "string",
+  //   "mnemonic": "string"
 
-//   "customer_id": "string",
-//   "walletAddress": "string",
-//   "secKey": "string",
-//   "mnemonic": "string"
-
-
-try {
-    
+  try {
     //     address: "0xdd8E233F2c7Ec65e480A3AeC2315a5C13Dcd1ba6"
     // key: "0xff11c7691a7cf12f3a1944ce57fb4a3fc50cb23a53d607ea489c4dc38d6524e4"
     // mnemonic: "brief theme connect fish order point unusual family edit off purity ill"
-   
 
     const res = await axios.get(
       api_url3 + "/api/transactions/create/wallet",
@@ -43,44 +37,28 @@ try {
       config
     );
     // console.log(res.data);
+    const payload = JSON.stringify({ customer_id, tokenSymbol });
 
-    
-    // /v1/wallet/create/wallet
-    if (res.data !== undefined) {
+    // console.log(payload);
 
-        const walletAddress = res.data.address
-        const secKey = res.data.key
-        const mnemonic = res.data.mnemonic
+    let res6 = await axios.post(
+      api_url2 + "/v1/wallet/create/wallet",
+      payload,
+      config
+    );
 
-        const payload = JSON.stringify({customer_id, walletAddress, secKey, mnemonic});
-  
-        // console.log(payload);
+    // console.log(res6);
 
-
-        let res6 = await axios.post(api_url2+"/v1/wallet/create/wallet", payload, config)
-
-        // console.log(res6);
-
-        return {
-            success: true,
-            data: res6.data,
-            address: walletAddress
-          };
-
-
-    } else {
-        return {
-            success: false,
-            data: res.data,
-          };
-    }
-   
+    return {
+      success: true,
+      data: res6.data,
+      // address: walletAddress,
+    };
   } catch (error) {
     console.log(error);
     return {
-        success: false,
-        data: error.response,
-      };
+      success: false,
+      data: error.response,
+    };
   }
 };
-
