47e13f3a8cc01f5366a3a437b862bff3295e3016
System Administrator <samuelify225@gmail.com>
2022-02-23 16:23:07 +0100

    update

--- client/src/flutterwave/API/initializePayment.js
+++ client/src/flutterwave/API/initializePayment.js
@@ -1,50 +1,41 @@
 // import axios from '../Axios'
+import { WindowSharp } from '@mui/icons-material';
 import axios from 'axios';
 import { API_URL2 } from '../../actions/types';
-const initializePayment = async (amount) => {
+const initializePayment = async (amount, customer_data) => {
 
     const security = "FLWSECK-26a59517b4cd2e97b661af9868a2e9ab-X";
-    const flutterConfig = {
-        public_key: 'FLWPUBK-bb7997b5dc41c89e90ee4807684bd05d-X',
-        tx_ref: Date.now(),
-        amount,
-      
-        currency: 'NGN',
-        // redirect_url: "https://a3dc-197-210-85-62.ngrok.io/v1/webhooks/all",
-        payment_options: "card",
-        // payment_plan:63558,
-        customer: {
-          phonenumber: '0810',
-        email:'hitechsuite@gmail.com', 
-          name: "kingsley goodluck Chiadi", 
-        },
-        customizations: {
-          title: "Payment from Egoras savings",
-          description: 'Payment for items in cart',
-          logo: 'https://egoras.com/img/egoras-logo.svg',
-        },
-      };
-
+    const {email, phonenumber, name } = customer_data; 
       const body = {
-
-        amount
+        amount, 
+        email, phonenumber, 
+        name
       }
 
-
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        }
+      };
 
     try {
-        const call = await axios.post(`${API_URL2}/v1/flutter/payments/initialize`, body , {headers: {
-            // Authorization:  `Bearer ${security}`, 
-            "Content-Type": "application/json"
-        }})
+        const call = await axios.post(`${API_URL2}/v1/flutter/payments/initialize`, body , config)
+       
         if (!call){
             return; 
         }
         console.log(call, "from the anonymous")
+        const {link} = call.data.data.data;
+        console.log(link, 'from me ')
+        window.open(link, "_self")
        return call; 
        
     } catch (err) {
-        console.log(err.message)
+        console.log(err.response)
+        return err.response
+
+        //just checking
+        
     }
 
 
