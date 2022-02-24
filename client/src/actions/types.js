eff788194556dec61b87946e94f2599e06bbe226
Kingsley GoodluK <goodluckcanhelp@gmail.com>
2022-02-24 16:06:20 +0100

    make do

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -30,11 +30,11 @@ export const API_URL = "https://tea.egoras.com";
 // export const API_URL2 = "https://saul.egoras.com";
 // export const API_URL3 = "https://coffee.egoras.com";
 // export const API_URL3 = "http://localhost:7976";
-// export const API_URL2 = "http://localhost:4000";
+// export const API_URL2 = "http://localhost:4000";     
 
 // export const API_URL3 = "http://localhost:7976";
-export const API_URL2 = "https://saul.egoras.com";
-// export const API_URL2 = "http://192.168.1.63:4000";
+// export const API_URL2 = "https://saul.egoras.com";
+export const API_URL2 = "http://192.168.43.179:4000";
 
 // token Symbol
 // constract address
