d7b0aa671021f1abfb817578f8169993d5fd802b
Kingsley GoodluK <goodluckcanhelp@gmail.com>
2022-03-03 15:03:29 +0100

    added reges for commas

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -28,9 +28,9 @@ export const API_URL = "https://tea.egoras.com";
 
 
 
-export const API_URL2 = "https://saul.egoras.com";
+// export const API_URL2 = "https://saul.egoras.com";
 
-// export const API_URL2 = "http://localhost:4000";
+export const API_URL2 = "http://localhost:4000";
 // export const API_URL2 = "http://192.168.43.179:4000";
 
 // token Symbol
