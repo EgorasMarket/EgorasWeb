62d43196ca1fe176d4c1bbde2657394ca5d81d15
Kingsley GoodluK <goodluckcanhelp@gmail.com>
2022-03-03 14:10:57 +0100

    message

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -26,9 +26,12 @@ export const ADD_TO_CART = "ADD_TO_CART";
 
 export const API_URL = "https://tea.egoras.com";
 
+
+
 export const API_URL2 = "https://saul.egoras.com";
 
 // export const API_URL2 = "http://localhost:4000";
+// export const API_URL2 = "http://192.168.43.179:4000";
 
 // token Symbol
 // constract address
